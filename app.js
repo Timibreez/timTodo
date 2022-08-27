5752b13fb749150281b1f7da9728bd4f11d5dfac
Timibreez <ffftimi@gmail.com>
2022-08-27 15:36:31 +0100

    Todo Model: created and saved Todo model

--- app.js
+++ app.js
@@ -10,18 +10,43 @@ app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.connect('mongodb://localhost:27017/todoDB')
+mongoose.connect('mongodb://localhost:27017/todoDB', {useNewUrlParser: true})
 
 const requestSchema = mongoose.Schema({
     item: String
 })
-const Request = mongoose.model('Request', requestSchema)
+
+const Todo = mongoose.model('Request', requestSchema)
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index')
+    Todo.find((err, found) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.render('index', {todos: found})
+        }
+    })
+})
+
+app.post('/', (req, res) => {
+    const input = req.body.input
+
+    const todo = new Todo({
+        item: input
+    })
+
+    todo.save((err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.redirect('/')
+        }
+    })
 })
 
 app.listen(PORT, () => {
-    console.log('running on port|:|' + PORT)
+    console.log('listening to the server on http://localhost:3000')
 })
