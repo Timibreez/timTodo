5533dedd67b7f3be041e7612dd12092ed2e46b6e
Timibreez <ffftimi@gmail.com>
2022-08-27 12:44:59 +0100

    app: created model and route to homepage

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+app.connect('mongodb://localhost:27017/todoDB')
+
+const requestSchema = mongoose.Schema({
+    item: String
+})
+const Request = mongoose.model('Request', requestSchema)
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
 app.listen(PORT, () => {
     console.log('running on port|:|' + PORT)
 })
